e72f31b199193fb6120a84f4ae609469403e8c48
Ellen Zidane <51652161+EllenZidane@users.noreply.github.com>
2021-06-11 09:08:52 -0300

    Add files via upload

---
+++ JAVA/Atividade 6.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+public class Exercicio6 {
+    public static void main(String[] args){
+
+        System.out.print("Digite o primeiro numero: ");
+        Scanner scanner = new Scanner(System.in);
+
+        int n01 = scanner.nextInt();
+        System.out.print("Digite o segundo numero: ");
+        int n02 = scanner.nextInt();
+        System.out.print("Digite o terceiro numero: ");
+        int n03 = scanner.nextInt();
+        System.out.print("Digite o quarto numero: ");
+        int n04 = scanner.nextInt();
+        System.out.print("Digite o quinto numero: ");
+        int n05 = scanner.nextInt();
+        System.out.print("Digite o sexto numero: ");
+        int n06 = scanner.nextInt();
+        System.out.print("Digite o setimo numero: ");
+        int n07 = scanner.nextInt();
+        System.out.print("Digite o oitavo numero: ");
+        int n08 = scanner.nextInt();
+        System.out.print("Digite o nono numero: ");
+        int n09 = scanner.nextInt();
+        System.out.print("Digite o decimo numero: ");
+        int n10 = scanner.nextInt();
+
+        System.out.print("A soma dos primeiros numero e: " + (n01+n02+n03+n04+n05+n06+n07+n08+n09+n10));
+    }
+}
---
+++ JAVA/Atividade 7.java
@@ -0,0 +1,82 @@
+import java.util.Scanner;
+public class Exercicio7 {
+    public static void main(String[] args){
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um numero");
+        int n01 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n02 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n03 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n04 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n05 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n06 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n07 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n08 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n09 = scanner.nextInt();
+        
+        System.out.print("Digite um numero");
+        int n10 = scanner.nextInt();
+
+        int soma=0;
+  if(n01<40) {
+	  soma+=n01;
+ 
+  }
+  if(n02<40) {
+	  soma+=n02;
+ 
+  }
+  if(n03<40) {
+	  soma+=n03;
+ 
+  }
+  if(n04<40) {
+	  soma+=n04;
+ 
+  }
+  if(n05<40) {
+	  soma+=n05;
+ 
+  }
+  if(n06<40) {
+	  soma+=n06;
+ 
+  }
+  if(n07<40) {
+	  soma+=n07;
+ 
+  }
+  if(n08<40) {
+	  soma+=n08;
+ 
+  }
+  if(n09<40) {
+	  soma+=n09;
+ 
+  }
+  if(n10<40) {
+	  soma+=n10;
+ 
+  }
+  
+	
+	System.out.println("A soma dos numeros menores que 40 e : "+ soma);
+}
+   
+}
